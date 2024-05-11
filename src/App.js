8fdcec1bcd136fb4bd0e2ec2d38b43b74b6d55a1
lythanh24 <159885417+lythanh24@users.noreply.github.com>
2024-05-11 17:16:47 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
-
+import * as React from 'react';
 function App() {
+  const name = "Robert Lee";
+
+  const welcome ={
+    greeting: `Hey`,
+    tile: `React`,
+  };
+
+  function getTile(title){
+    return title;
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hello World {name}</h1>
+      <h2>{welcome.greeting} {welcome.tile}</h2>
+      <h2>{getTile("Mr.")} {welcome.tile}</h2>
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text" />
     </div>
   );
 }
-
 export default App;
