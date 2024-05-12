56d173a1fcef555d3cd89380b5956625bd4b5f12
lythanh24 <159885417+lythanh24@users.noreply.github.com>
2024-05-12 16:03:59 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import * as React from 'react';
 
+var count, setCount;
+
 const name = "Robert Lee";
 
 const welcome = {
@@ -40,10 +42,10 @@ const list = [
   },
 ];
 
-function List() {
+function List(props) {
   return (
     <ul>
-      {list.map(function (item) {
+      {props.list.map(function (item) {
         return (
           <li key={item.objectID}>
             <span>
@@ -68,39 +70,98 @@ function Search() {
   );
 }
 
+const SearchForm = (props) => {
+
+  const [searchTerm, setSearchTerm] = React.useState(0);
+
+  const handleChange = (event) => {
+    setSearchTerm(event.target.value);
+    // B
+    props.onSearch(event);
+  };
+  
+  const handleClick = (event) => {
+    setCount(count + 1);
+    console.log("Button Clicked");
+  }
+  
+  return (
+    <div>
+      Count: {count}
+      <label htmlFor="search">Form: </label>
+      <input id="search" type="text" onChange={handleChange} />
+      <button type="button" onClick={handleClick}>
+        Event Handler
+      </button>
+    </div>
+  );
+}
+
+
+
 function App() {
+
+  [count, setCount] = React.useState(0);
+
+  const stories = [
+    {
+      title: 'ReactJS',
+      url: 'https://reactjs.org/',
+      author: 'Jordan Walke',
+      num_comments: 3,
+      points: 4,
+      objectID: 0,
+    },
+    {
+      title: 'Redux',
+      url: 'https://redux.js.org/',
+      author: 'Dan Abramov, Andrew Clark',
+      num_comments: 2,
+      points: 5,
+      objectID: 1
+    }, {
+      title: 'Redhad',
+      url: 'https://redux.js.org/',
+      author: 'Linux Toward',
+      num_comments: 2,
+      points: 5,
+      objectID: 1
+    }, {
+      title: 'Cup',
+      url: 'https://redux.js.org/',
+      author: 'Ceramix',
+      num_comments: 2,
+      points: 5,
+      objectID: 1
+    },
+  ];
 
   function getTile(title) {
     return title;
   };
 
+  const handleSearch = (event) => {
+    console.log("App ",event.target.value);
+  }
+
   return (
     <div>
       <h1>Hello World {name}</h1>
+      <h2>Count: {count}</h2>
       <h2>{welcome.greeting} {welcome.tile}</h2>
       <h2>{getTile("Mr.")} {welcome.tile}</h2>
-      <hr/>
-      <Search/>
+      <hr />
+      <Search />
 
       <hr />
-      <List />
+
+      <SearchForm onSearch={handleSearch} />
 
       <hr />
-      <ul>
-        {list.map(function (item) {
-          return (
-            <li>
-              <span>
-                <a href={item.url}>{item.title}</a>
-              </span>
-              <span> {item.author}</span>
-              <span> {item.num_comments}</span>
-              <span> {item.points}</span>
-            </li>
-          );
 
-        })}
-      </ul>
+      <List list={stories} />
+
+      <hr />
 
     </div>
   );
